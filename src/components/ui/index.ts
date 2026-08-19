5ae8476561e186dc3763ac354ef94daff52cf1c4
kelvinessuman <kelvinessuman0601@gmail.com>
2026-08-19 21:26:46 +0000

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Alert } from "./Alert";
+export { Spinner } from "./Spinner";
+export { EmptyState } from "./EmptyState";
+export { Card, CardHeader } from "./Card";
